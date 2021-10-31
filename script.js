1d2637fde2c6fe93b13f9cf722db434392c33b34
safiulrehman <safiulrehman@gmail.com>
2021-11-01 00:44:57 +0300

    project-updated

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+var principal,num_of_year,rate,total,amount;
+principal = document.getElementById ("principal").value;
+num_of_year = document.getElementById ("years").value;
+rate = document.getElementById ("rate").value;
+
+if(principal=='' || rate=='' ||num_of_year==''){
+	alert('Please fill the values.');
 }
+else{
+total = parseInt((principal*num_of_year*rate)/100 );
+amount = principal*Math.pow((1 +rate/100),num_of_year );
+
+const date = new Date(new Date().setFullYear(new Date().getFullYear() + (+num_of_year)));
+let year = date.getFullYear();
+
+document.getElementById ('amnt').innerHTML ="If you deposit "+principal+",<br>";
+document.getElementById ('rat').innerHTML ="at an interest rate of : "+rate+"%.<br>";
+document.getElementById ('total').innerHTML ="You will receive an amount of "+total+",<br>";
+document.getElementById ('nextyear').innerHTML ="in the year "+year;
+}
+}
+
+function SetDefaultValue() {
+var PopulateField = document.getElementById("sliderselect").value="10.50";
+}	
+window.onload = function(){
+    SetDefaultValue();
+};
+
+
         
